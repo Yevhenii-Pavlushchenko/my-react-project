ade1d13cfc980b51e558cdbd618a81c6b449a46e
JustFun <paw93@ukr.net>
2026-03-06 09:30:34 +0200

    ++

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 // src/main.tsx
 
-import React from "react"
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
-// import "modern-normalize";
-// import "./global.css";
 
+import "modern-normalize";
+import "./global.css";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </React.StrictMode>);
-
+  </React.StrictMode>
+);
